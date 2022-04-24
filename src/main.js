bd4ef18b74014eb8e20712278226f5afa5c490f5
Gavin Kimpson <gkimpson@gmail.com>
2022-04-24 16:52:32 +0100

    vue router working & cleaned up existing hard-coded routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from "@/components/PageHome";
+import * as VueRouter from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: PageHome
+  }
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+})
 
 const forumApp = createApp(App)
+forumApp.use(router)
 forumApp.mount('#app')
